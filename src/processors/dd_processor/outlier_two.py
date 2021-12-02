1a4e73720233d5da8f2a6202b5debe0dc94128ec
Afzal <afzalkhanahmed@gmail.com>
2021-12-02 15:50:05 +0530

    Update outlier_two.py

--- src/processors/dd_processor/outlier_two.py
+++ src/processors/dd_processor/outlier_two.py
@@ -1,5 +1,7 @@
 from __future__ import division
 from re import escape
+from typing import List
+from numpy.core.arrayprint import format_float_positional
 
 from numpy.core.fromnumeric import reshape, var
 from numpy.core.function_base import linspace
@@ -31,6 +33,7 @@ from datetime import date, timedelta
 #from pysimplelog import Logger
 import string
 from dateutil.relativedelta import relativedelta
+from src.processors.config_extractor.outlier import CheckOutlier
 
 client = MongoClient("mongodb://localhost:27017/aranti")
 db=client.get_database("aranti")
@@ -77,19 +80,22 @@ class OutlierTwo():
     def dataframe_generator(self,identifier,main_data_dict,processed_daily_data,no_months,last_year_months):
         if type(main_data_dict['processed'])!=str:
             current_date=processed_daily_data['rep_dt']['processed'].date()
-            current_date=current_date+relativedelta(days=1)
             # print(current_date)
             # current_date=datetime.datetime(2016, 7, 16)#to be changed later only temporary use
             if no_months!=0:
+                current_date=current_date+relativedelta(days=1)
                 old_month=current_date-relativedelta(months=no_months)
                 list_date=pd.date_range(old_month,current_date,freq='d')
-                dataframe=self.time_dataframe_generator(identifier,main_data_dict,no_months,last_year_months,list_date)
+                dataframe_final=self.time_dataframe_generator(identifier,main_data_dict,no_months,last_year_months,list_date)
             elif last_year_months!=0:
                 last_year_current_month=current_date-relativedelta(months=12)
                 last_year_prev_months=last_year_current_month-relativedelta(months=last_year_months)
                 list_date=pd.date_range(last_year_prev_months,last_year_current_month,freq='d')
                 dataframe=self.time_dataframe_generator(identifier,main_data_dict,no_months,last_year_months,list_date)
-        return dataframe
+                list_date_2=pd.date_range(current_date,current_date+relativedelta(days=1),freq='d')
+                dataframe_2=self.time_dataframe_generator(identifier,main_data_dict,no_months,last_year_months,list_date_2)
+                dataframe_final=dataframe_2.append(dataframe)
+        return dataframe_final
 
 
     def outlier_processor(self,identifier,dataframe,main_data_dict,processed_daily_data):
@@ -99,164 +105,224 @@ class OutlierTwo():
         current_date=processed_daily_data['rep_dt']['processed'].date()
         dataframe=dataframe.dropna()
         dataframe=dataframe.reset_index(drop=True)
+        dataframe=dataframe[dataframe[identifier]!='']
+        dataframe=dataframe[dataframe[identifier]!=' ']
+        dataframe=dataframe[dataframe[identifier]!='  ']
+        dataframe=dataframe.reset_index(drop=True)
         # z_score_limit=st.norm.ppf(1-i)
+        # print(dataframe)
+        # exit()
         if len(dataframe)>=2:
             dataframe["z_score"]=st.zscore(dataframe[identifier])
             for i in range(0,len(dataframe['rep_dt'])):
                 if dataframe['rep_dt'][i]==current_date:
-                    z_score=dataframe['z_score'][i]
-                    
+                    try:
+                        z_score=dataframe['z_score'][i]
+                        return z_score,True
+                    except:
+                        return None,False  
         else:
-            outlier=None
-            operational=None
-            z_score=None
-            return outlier,operational,z_score
+            return None,False
+                 
+
+    def outlier_operational_z_score(self,z_score,z_score_limit):
+        outlier_z_score={}
+        if pd.isnull(z_score['m3']):
+            outlier_z_score['m3']=None
+        else:
+            if z_score['m3']>=-z_score_limit and z_score['m3']<=z_score_limit:
+                outlier_z_score['m3']=True
+            else:
+                outlier_z_score['m3']=False
+
+        if pd.isnull(z_score['m6']):
+            outlier_z_score['m6']=None
+        else:
+            if z_score['m6']>=-z_score_limit and z_score['m6']<=z_score_limit:
+                outlier_z_score['m6']=True
+            else:
+                outlier_z_score['m6']=False
+
+        if pd.isnull(z_score['m12']):
+            outlier_z_score['m12']=None
+        else:
+            if z_score['m12']>=-z_score_limit and z_score['m12']<=z_score_limit:
+                outlier_z_score['m12']=True
+            else:
+                outlier_z_score['m12']=False
+
+        if pd.isnull(z_score['ly_m3']):
+            outlier_z_score['ly_m3']=None
+        else:
+            if z_score['ly_m3']>=-z_score_limit and z_score['ly_m3']<=z_score_limit:
+                outlier_z_score['ly_m3']=True
+            else:
+                outlier_z_score['ly_m3']=False
+
+        if pd.isnull(z_score['ly_m6']):
+            outlier_z_score['ly_m6']=None
+        else:
+            if z_score['ly_m6']>=-z_score_limit and z_score['ly_m6']<=z_score_limit:
+                outlier_z_score['ly_m6']=True
+            else:
+                outlier_z_score['ly_m6']=False
+
+        if pd.isnull(z_score['ly_m12']):
+            outlier_z_score['ly_m12']=None
+        else:
+            if z_score['ly_m12']>=-z_score_limit and z_score['ly_m12']<=z_score_limit:
+                outlier_z_score['ly_m12']=True
+            else:
+                outlier_z_score['ly_m12']=False
+        return outlier_z_score
+
+    
+    
+        # else:
+        #     outlier=None
+        #     operational=None
+        #     z_score=None
+        #     return outlier,operational,z_score
 
             
-        if pd.isnull(limit_val) or (limit_val!=1 and limit_val!=2 and limit_val!=3):
-            limit_val=2
-        
-        if limit_val==1:
-            if outlier_val==True:
-                outlier=True
+        # if pd.isnull(limit_val) or (limit_val!=1 and limit_val!=2 and limit_val!=3):
+        #     limit_val=2
+       
+        # if limit_val==1:
+        #     if outlier_val==True:
+        #         outlier=True
 
-            elif outlier_val==False:
-                outlier=False
+        #     elif outlier_val==False:
+        #         outlier=False
                 
-            elif outlier_val!=True and outlier_val!=False:
-                outlier="not checked"
+        #     elif outlier_val!=True and outlier_val!=False:
+        #         outlier="not checked"
                 
-            if operational_val==True:
-                operational=True
+        #     if operational_val==True:
+        #         operational=True
                 
-            elif operational_val==False:
-                operational=False
+        #     elif operational_val==False:
+        #         operational=False
                 
-            elif operational_val!=True and operational_val!=False:
-                operational="not checked"
+        #     elif operational_val!=True and operational_val!=False:
+        #         operational="not checked"
 
-            if pd.isnull(z_score)==True:
-                z_score=None
+        #     if pd.isnull(z_score)==True:
+        #         z_score=None
                 
-            return outlier,operational,z_score
+        #     return outlier,operational,z_score
 
-        elif limit_val==2:
-            if outlier_val==True:
-                outlier=True
+        # elif limit_val==2:
+        #     if outlier_val==True:
+        #         outlier=True
                 
-            elif outlier_val==False:
-                outlier=False
+        #     elif outlier_val==False:
+        #         outlier=False
                 
-            elif outlier_val!=True and outlier_val!=False:
-                if pd.isnull(z_score)==False:
-                    if z_score<=3 and z_score>=-3:
-                        outlier=True
-                    else:
-                        outlier=False
+        #     elif outlier_val!=True and outlier_val!=False:
+        #         if pd.isnull(z_score)==False or z_score!=None:
+        #             if z_score<=3 and z_score>=-3:
+        #                 outlier=True
+        #             else:
+        #                 outlier=False
                    
-            if operational_val==True:
-                operational=True
+        #     if operational_val==True:
+        #         operational=True
                 
-            elif operational_val==False:
-                operational=False
+        #     elif operational_val==False:
+        #         operational=False
                 
-            elif operational_val!=True and operational_val!=False:
-                if pd.isnull(z_score)==False:
-                    if z_score<=2 and z_score>=-2:
-                        operational=True
-                    else:
-                        operational=False
+        #     elif operational_val!=True and operational_val!=False:
+        #         if pd.isnull(z_score)==False or z_score!=None:
+        #             if z_score<=2 and z_score>=-2:
+        #                 operational=True
+        #             else:
+        #                 operational=False
 
-            if pd.isnull(z_score)==True:
-                z_score=None
+        #     if pd.isnull(z_score)==True:
+        #         z_score=None
 
-            return outlier,operational,z_score 
+        #     return outlier,operational,z_score 
 
-        elif limit_val==3:
-            if outlier_val==True:
-                outlier=True
+        # elif limit_val==3:
+        #     if outlier_val==True:
+        #         outlier=True
                 
-            elif outlier_val==False:
-                outlier=False
+        #     elif outlier_val==False:
+        #         outlier=False
                 
-            if pd.isnull(z_score)==False:
-                if z_score<=3 or z_score>=-3:
-                    z_outlier=True
-                else:
-                    z_outlier=False
+        #     if pd.isnull(z_score)==False:
+        #         if z_score<=3 or z_score>=-3:
+        #             z_outlier=True
+        #         else:
+        #             z_outlier=False
                    
-            if operational_val==True:
-                operational=True
+        #     if operational_val==True:
+        #         operational=True
                 
-            elif operational_val==False:
-                operational=False
+        #     elif operational_val==False:
+        #         operational=False
            
-            if pd.isnull(z_score)==False:
-                if z_score<=2 and z_score>=-2:
-                    z_operational=True
-                else:
-                    z_operational=False
+        #     if pd.isnull(z_score)==False:
+        #         if z_score<=2 and z_score>=-2:
+        #             z_operational=True
+        #         else:
+        #             z_operational=False
             
-            if pd.isnull(z_score)==True:
-                z_outlier=None
-                z_operational=None
+        #     if pd.isnull(z_score)==True:
+        #         z_outlier=None
+        #         z_operational=None
 
-            if z_outlier!=None:
-                if z_outlier==outlier:
-                    outlier=True
-                else:
-                    outlier=False
+        #     if z_outlier!=None:
+        #         if z_outlier==outlier:
+        #             outlier=True
+        #         else:
+        #             outlier=False
             
-                if z_operational==operational:
-                    operational=True
-                else:
-                    operational=False
+        #         if z_operational==operational:
+        #             operational=True
+        #         else:
+        #             operational=False
             
-            return outlier,operational,z_score 
+        #     return outlier,operational,z_score 
 
 
 
 
 
     def base_prediction_processor(self,identifier,main_data_dict,processed_daily_data):
-            main_data_dict=main_data_dict
-            outlier={}
-            operational={}
-            z_score={}
-            m3_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,3,0)
-            outlier_m3,operational_m3,z_score_m3=self.outlier_processor(identifier,m3_dataframe,main_data_dict,processed_daily_data)
-            outlier['m3']=outlier_m3
-            operational['m3']=operational_m3
-            z_score['m3']=z_score_m3
-            
-            m6_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,6,0)
-            outlier_m6,operational_m6,z_score_m6=self.outlier_processor(identifier,m6_dataframe,main_data_dict,processed_daily_data)
-            outlier['m6']=outlier_m6
-            operational['m6']=operational_m6
-            z_score['m6']=z_score_m6
-           
-            m12_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,12,0)
-            outlier_m12,operational_m12,z_score_m12=self.outlier_processor(identifier,m12_dataframe,main_data_dict,processed_daily_data)
-            outlier['m12']=outlier_m12
-            operational['m12']=operational_m12
-            z_score['m12']=z_score_m12
-            
-            ly_m3_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,3)
-            outlier_ly_m3,operational_ly_m3,z_score_ly_m3=self.outlier_processor(identifier,ly_m3_dataframe,main_data_dict,processed_daily_data)
-            outlier['ly_m3']=outlier_ly_m3
-            operational['ly_m3']=operational_ly_m3
-            z_score['ly_m3']=z_score_ly_m3
-            
-            ly_m6_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,6)
-            outlier_ly_m6,operational_ly_m6,z_score_ly_m6=self.outlier_processor(identifier,ly_m6_dataframe,main_data_dict,processed_daily_data)
-            outlier['ly_m6']=outlier_ly_m6
-            operational['ly_m6']=operational_ly_m6
-            z_score['ly_m6']=z_score_ly_m6
-            
-            ly_m12_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,12)
-            outlier_ly_m12,operational_ly_m12,z_score_ly_m12=self.outlier_processor(identifier,ly_m12_dataframe,main_data_dict,processed_daily_data)
-            outlier['ly_m12']=outlier_ly_m12
-            operational['ly_m12']=operational_ly_m12
-            z_score['ly_m12']=z_score_ly_m12
-            
-            return outlier,operational,z_score
+        main_data_dict=main_data_dict
+        data_availabe={}
+        z_score={}
+        m3_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,3,0)
+        z_score_m3,data_availabe_m3=self.outlier_processor(identifier,m3_dataframe,main_data_dict,processed_daily_data)
+        z_score['m3']=z_score_m3
+        data_availabe['m3']=data_availabe_m3
+        
+        m6_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,6,0)
+        z_score_m6,data_availabe_m6=self.outlier_processor(identifier,m6_dataframe,main_data_dict,processed_daily_data)
+        z_score['m6']=z_score_m6
+        data_availabe['m6']=data_availabe_m6
+
+        
+        m12_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,12,0)
+        z_score_m12,data_availabe_m12=self.outlier_processor(identifier,m12_dataframe,main_data_dict,processed_daily_data)
+        z_score['m12']=z_score_m12
+        data_availabe['m12']=data_availabe_m12
+          
+        ly_m3_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,3)
+        z_score_ly_m3,data_availabe_ly_m3=self.outlier_processor(identifier,ly_m3_dataframe,main_data_dict,processed_daily_data)
+        z_score['ly_m3']=z_score_ly_m3
+        data_availabe['ly_m3']=data_availabe_ly_m3
+        
+        ly_m6_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,6)
+        z_score_ly_m6,data_availabe_ly_m6=self.outlier_processor(identifier,ly_m6_dataframe,main_data_dict,processed_daily_data)
+        z_score['ly_m6']=z_score_ly_m6
+        data_availabe['ly_m6']=data_availabe_ly_m6
+        
+        ly_m12_dataframe=self.dataframe_generator(identifier,main_data_dict,processed_daily_data,0,12)
+        z_score_ly_m12,data_availabe_ly_m12=self.outlier_processor(identifier,ly_m12_dataframe,main_data_dict,processed_daily_data)        
+        z_score['ly_m12']=z_score_ly_m12
+        data_availabe['ly_m12']=data_availabe_ly_m12
+
+        return z_score,data_availabe
