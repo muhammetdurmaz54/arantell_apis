8597232d98ef249d31c70d576fef03d5561621db
Afzal <afzalkhanahmed@gmail.com>
2022-08-16 11:21:28 +0530

    Update overview_process.py

--- src/processors/config_extractor/overview_process.py
+++ src/processors/config_extractor/overview_process.py
@@ -5,19 +5,21 @@ import pandas as pd
 import numpy as np
 
 class OverviewExtractor():
-    def __init__(self):
-        pass
+    def __init__(self, id):
+        self.id = id
 
     def process_data(self):
         self.configuration = Configurator(9591301)
         self.ship_configs = self.configuration.get_ship_configs()
         self.main_db = self.configuration.get_main_data()
 
-        imo_list = self.configuration.create_imo_and_name_strings()
-        vessel_load_list = self.configuration.create_vessel_load_list()
-        eta_list = self.configuration.create_eta_list()
+        imo_list = self.configuration.create_imo_and_name_strings(self.id)
+        vessel_load_list = self.configuration.create_vessel_load_list(self.id)
+        eta_list = self.configuration.create_eta_list(self.id)
+        # print("VESSEL LOAD DICT", vessel_load_list)
+        # print("ETA DICT", eta_list)
         # cp_compliance_list = self.configuration.create_cp_compliance_list()
-        active_ships = self.configuration.create_list_of_active_ships()
+        active_ships = self.configuration.create_list_of_active_ships(self.id)
         total_ships = len(active_ships)
 
         actual_active_ships = 0
@@ -27,8 +29,8 @@ class OverviewExtractor():
 
         daily_data_received = str(int((actual_active_ships * 100) / total_ships)) + "%"
 
-        total_issues, issuesCount = self.configuration.create_dict_of_issues('')
-        cp_compliance_dict = self.configuration.create_cp_compliance_dict()
+        total_issues, issuesCount = self.configuration.create_dict_of_issues(self.id, '')
+        cp_compliance_dict = self.configuration.create_cp_compliance_dict(self.id)
         print("COMPLIANCE", cp_compliance_dict)
         print("ISSUES COUNT", issuesCount)
 
@@ -37,10 +39,10 @@ class OverviewExtractor():
 
         for i in range(len(imo_list)):
             try:
-                vessel_load = vessel_load_list[i]
-                eta = eta_list[i]
-                # len_of_cp_dict = len(list(set(list(cp_compliance_dict[].values())))) == 1
                 tempImo = imo_list[i].split('-')[0]
+                vessel_load = vessel_load_list[int(tempImo)] if int(tempImo) in vessel_load_list else None
+                eta = eta_list[int(tempImo)] if int(tempImo) in eta_list else None
+                # len_of_cp_dict = len(list(set(list(cp_compliance_dict[].values())))) == 1
                 try:
                     for cp in cp_compliance_dict[int(tempImo)]:
                         if cp['compliant'] == "Yes":
@@ -55,14 +57,30 @@ class OverviewExtractor():
                 eta = None
             result[i] = {'imo_string': imo_list[i], 'vessel_load': vessel_load, 'eta': eta, 'cp': cp_compliance}
         
-        for key in total_issues.keys():
-            for i in result.keys():
-                tempImo = result[i]['imo_string'].split('-')[0]
-                if key == tempImo:
-                    result[i]['issues'] = total_issues[key]
-                    total_issues_number = total_issues_number + issuesCount[int(key)]['outlier'] + issuesCount[int(key)]['operational']
-                    issueText = str(issuesCount[int(key)]['outlier']) + ' of Outliers, ' + str(issuesCount[int(key)]['operational']) + ' of Operational.'
-                    result[i]['issuesCount'] = issueText
+        # for key in total_issues.keys():
+        #     for i in result.keys():
+        #         tempImo = result[i]['imo_string'].split('-')[0]
+        #         if int(key) == int(tempImo):
+        #             # result[i]['issues'] = total_issues[key]
+        #             print("NUMBER!!!!!!!", i)
+        #             total_issues_number = total_issues_number + issuesCount[int(key)]['outlier'] + issuesCount[int(key)]['operational']
+        #             issueText = str(issuesCount[int(key)]['outlier']) + ' of Outliers, ' + str(issuesCount[int(key)]['operational']) + ' of Operational.'
+        #             # print(issuesCount[int(key)]['outlier'], issuesCount[int(key)]['operational'], issueText)
+        #             print(result[i])
+        #             result[i]['issuesCount'] = issueText
+        #         else:
+        #             result[i]['issuesCount'] = None
+
+        for i in result.keys():
+            tempImo = result[i]['imo_string'].split('-')[0]
+            if tempImo in list(total_issues.keys()):
+                total_issues_number = total_issues_number + issuesCount[int(tempImo)]['outlier'] + issuesCount[int(tempImo)]['operational']
+                issueText = str(issuesCount[int(tempImo)]['outlier']) + ' of Outliers, ' + str(issuesCount[int(tempImo)]['operational']) + ' of Operational.'
+                # print(issuesCount[int(key)]['outlier'], issuesCount[int(key)]['operational'], issueText)
+                print(result[i])
+                result[i]['issuesCount'] = issueText
+            else:
+                result[i]['issuesCount'] = None
         
         return result, actual_active_ships, total_ships, daily_data_received, total_issues_number
 
