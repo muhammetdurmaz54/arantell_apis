b7ca5edd2692fd47efd7f07a66cf1249c46163b7
Afzal <afzalkhanahmed@gmail.com>
2023-04-27 12:14:10 +0530

    Create ais_read.py

---
+++ src/processors/dd_processor/ais_read.py
@@ -0,0 +1,103 @@
+import requests
+# import pandas
+# from bs4 import BeautifulSoup
+# import re
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+# from apscheduler.schedulers.background import BackgroundScheduler
+import time
+
+
+
+load_dotenv()
+
+MONGODB_URI = os.getenv('MONGODB_ATLAS')
+TOKEN_TYPE = os.getenv('AUTHORIZATION_TOKEN_TYPE')
+ACCESS_TOKEN = os.getenv('AUTHORIZATION_ACCESS_TOKEN')
+AUTHORIZATION_STRING = TOKEN_TYPE + " " + ACCESS_TOKEN
+client = MongoClient(MONGODB_URI)
+
+# client = MongoClient("mongodb://localhost:27017/aranti")
+db=client.get_database("aranti")
+database = db
+
+
+
+# print(AUTHORIZATION_STRING)
+
+def get_route(url, return_dict):
+    raw_json = requests.get(url,headers={'Authorization': AUTHORIZATION_STRING}).json()
+    # print(raw_json)
+    # print(raw_json['status'])
+    if raw_json['success']==True:
+        return_dict['departure_port'] = raw_json['data']['departure_port']
+        return_dict['departure_atd'] = raw_json['data']['departure_atd']
+        return_dict['arrival_port'] = raw_json['data']['arrival_port']
+        return_dict['arrival_atd'] = raw_json['data']['arrival_atd']
+        return_dict['get_route_success'] = True
+        return return_dict
+    else:
+        return_dict['departure_port'] = None
+        return_dict['departure_atd'] = None
+        return_dict['arrival_port'] = None
+        return_dict['arrival_atd'] = None
+        return_dict['get_route_success'] = False
+        return return_dict
+def get_current_position(url, return_dict):
+    raw_json = requests.get(url,headers={'Authorization': AUTHORIZATION_STRING}).json()
+    # print(raw_json)
+    # print(raw_json['status'])
+    if raw_json['success']==True:
+        latitude, longitude = raw_json['data']['latitude_longitude'].split(' / ')
+        speed, course = raw_json['data']['speed_course'].split(' / ')
+        position_received = raw_json['data']['position_received']
+        heading = None
+        return_dict['latitude'] = latitude
+        return_dict['longitude'] = longitude
+        return_dict['speed'] = speed
+        return_dict['course'] = course
+        return_dict['position_received'] = position_received
+        return_dict['heading'] = heading
+        return_dict['get_current_position_success'] = True
+
+        return return_dict
+    else:
+        heading = None
+        return_dict['latitude'] = None
+        return_dict['longitude'] = None
+        return_dict['speed'] = None
+        return_dict['course'] = None
+        return_dict['position_received'] = None
+        return_dict['heading'] = None
+        return_dict['get_current_position_success'] = False
+        return return_dict
+
+def job():
+    ais_collection=database.get_collection("Ais_collection")
+    return_dict = {}
+    return_dict['departure_port'] = None
+    return_dict['departure_atd'] = None
+    return_dict['arrival_port'] = None
+    return_dict['arrival_atd'] = None
+    return_dict['latitude'] = None
+    return_dict['longitude'] = None
+    return_dict['speed'] = None
+    return_dict['course'] = None
+    return_dict['position_received'] = None
+    return_dict['heading'] = None
+    return_dict = get_route("https://zylalabs.com/api/1835/vessel+information+and+route+tracking+api/1499/get+route?imoCode=9205926", return_dict)
+    return_dict = get_current_position('https://zylalabs.com/api/1835/vessel+information+and+route+tracking+api/1575/get+current+position?imoCode=9205926', return_dict)
+ 
+    ais_collection.insert_one(return_dict)
+
+
+# bs = BackgroundScheduler()
+# bs.add_job(job, trigger='interval', hours=2)
+# bs.start()
+
+while True:
+    print("STARTED")
+    job()
+    print("INSERTED")
+    time.sleep(120)
